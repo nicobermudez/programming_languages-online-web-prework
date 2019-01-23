f1177234838737bef90f566eeb045507e0085506
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 19:25:19 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
 def reformat_languages(languages)
   # your code here
+  languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+
+new_hash = {}
+languages.each do |style, data|
+  data.each do |key, values|
+    values.each do |k, v|
+      new_hash[key] = k
+    end
+  end
 end
+new_hash
+end
+
+
