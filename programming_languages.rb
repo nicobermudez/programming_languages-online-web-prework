f4318bb7b38cd02ea2d78842c48a6c8ee55f98c9
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 19:33:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -37,7 +37,8 @@ new_hash = {}
 languages.each do |style, data|
   data.each do |language, value|
     values.each do |details, v|
-      if new_hashes[language] != nil 
+      if new_hashes[language] = nil
+        new_hashes[language] = 
         
     end
   end
